07239e32b3a1684ff9a75c18f1761551cafaf671
Vasiliki Balidou <vasilikibalidou@gmail.com>
2020-02-10 14:03:44 +0100

    broken

---
+++ routes/books.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/books", (req, res, next) => {
+  console.log(req.query);
+  axios
+    .get(
+      `https://www.googleapis.com/books/v1/volumes?q=${req.query.bookSearch}`
+    )
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+module.exports = router;
