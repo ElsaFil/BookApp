325ac8deeb4bebb23220db4c2e032f7df1c45df4
Vasiliki Balidou <vasilikibalidou@gmail.com>
2020-02-11 16:35:02 +0100

    connected users with books

--- routes/books.js
+++ routes/books.js
@@ -42,28 +42,61 @@ router.post("/bookDetails/:bookGoogleId", (req, res, next) => {
       `https://www.googleapis.com/books/v1/volumes/${req.params.bookGoogleId}?key=${googleKey}`
     )
     .then(response => {
-      Book.create({
-        title: response.data.volumeInfo.title,
-        subtitle: response.data.volumeInfo.subtitle,
-        authors: response.data.volumeInfo.authors,
-        description: response.data.volumeInfo.description,
-        ISBN_10: response.data.volumeInfo.industryIdentifiers[0].identifier,
-        ISBN_13: response.data.volumeInfo.industryIdentifiers[1].identifier,
-        pageCount: response.data.volumeInfo.pageCount,
-        categories: response.data.volumeInfo.categories,
-        averageRating: response.data.volumeInfo.averageRating,
-        language: response.data.volumeInfo.language,
-        coverImageUrl: response.data.volumeInfo.imageLinks.thumbnail,
-        infoLinkURL: response.data.volumeInfo.infoLink,
-        googleId: req.params.bookGoogleId,
-        owners: req.user._id
-      })
-        .then(() => {
-          res.redirect(`/bookDetails/${req.params.bookGoogleId}`);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+      Book.findOne({ googleId: response.data.id }).then(foundBook => {
+        if (!foundBook) {
+          console.log("not in db");
+          Book.create({
+            title: response.data.volumeInfo.title,
+            subtitle: response.data.volumeInfo.subtitle,
+            authors: response.data.volumeInfo.authors,
+            description: response.data.volumeInfo.description,
+            ISBN_10: response.data.volumeInfo.industryIdentifiers[0].identifier,
+            ISBN_13: response.data.volumeInfo.industryIdentifiers[1].identifier,
+            pageCount: response.data.volumeInfo.pageCount,
+            categories: response.data.volumeInfo.categories,
+            averageRating: response.data.volumeInfo.averageRating,
+            language: response.data.volumeInfo.language,
+            coverImageUrl: response.data.volumeInfo.imageLinks.thumbnail,
+            infoLinkURL: response.data.volumeInfo.infoLink,
+            googleId: req.params.bookGoogleId,
+            owners: req.user._id
+          })
+            .then(createdBook => {
+              console.log(createdBook);
+              return User.updateOne(
+                { _id: req.user._id },
+                { $push: { books: createdBook._id } }
+              );
+            })
+            .then(() => res.redirect(`/bookDetails/${req.params.bookGoogleId}`))
+            .catch(err => {
+              console.log(err);
+            });
+        } else {
+          console.log("in db");
+          User.findById(req.user._id)
+            .then(foundUser => {
+              if (foundUser.books.includes(foundBook._id)) {
+                console.log("already exists");
+              } else {
+                console.log("new book for user");
+                return User.updateOne(
+                  { _id: req.user._id },
+                  { $push: { books: foundBook._id } }
+                ).then(() => {
+                  return Book.updateOne(
+                    { _id: foundBook._id },
+                    { $push: { owners: foundUser._id } }
+                  );
+                });
+              }
+            })
+            .then(() => res.redirect(`/bookDetails/${req.params.bookGoogleId}`))
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      });
     })
     .catch(err => {
       console.log(err);
